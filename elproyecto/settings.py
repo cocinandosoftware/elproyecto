0521edb0bd516127a455d4a5cb6f656fa8beede7
Sergi Garcia <sergi@ancoradual.com>
2025-08-15 00:37:31 +0200

    Sesión 17 y 18 - Trabajando entorno Web con Scss

--- elproyecto/settings.py
+++ elproyecto/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contextos.clientes',
-    'contextos.desarrolladores',
-    'contextos.backoffice',
+    'contextos',
     'core',
     'web', 
 ]
